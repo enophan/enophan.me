391c7fd2906f33cb22145ffad1f93a3d260dfa97
enophan <zhaooooooyifan@qq.com>
2022-07-11 16:46:12 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//TODO https://github.com/vuejs/vue/issues/5298
+
 const app = createApp(App)
 
 app.use(createPinia())
